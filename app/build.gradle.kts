f3ddfc6b5beb6f8675059b72becb9590948ff683
Tuyen <tuyenctbk@gmail.com>
2026-08-13 10:05:49 +0700

    chore: bump application version to 3.1.0 (versionCode 26)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.neotune.radio"
         minSdk = 24
         targetSdk = 35
-        versionCode = 25
-        versionName = "2.2.5"
+        versionCode = 26
+        versionName = "3.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
